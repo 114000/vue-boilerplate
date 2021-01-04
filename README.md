db125304382c99ddbbb55f858da4f2ce51c36fbd
binyu.wang <binyu.wang@zgcechuang.com>
2021-01-04 20:11:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# fe-vue-boilerplate
+
+vue 项目模板 
+
+- [x] typescript
+- [x] vue2
+- [x] vue-router
+- [x] vuex
+- [x] @vue/composition-api
+- [x] postcss
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} + {{ state.count }}</h1>
+    
+    <button @click="handleClick">log message</button>
+    <br>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { reactive, ref } from '@vue/composition-api'
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup (props, self) {
+    
+    const state = reactive({
+      count: ref(0)
+    })
+
+    function handleClick () {
+      state.count++
+      console.log('self', self)
+    }
+    return {
+      handleClick,
+      state
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
