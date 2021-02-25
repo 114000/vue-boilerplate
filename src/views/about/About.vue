50fc1eb80ff82abf43831e5c69dcf2c0b6dac2e2
binyu.wang <binyu.wang@zgcechuang.com>
2021-02-25 15:52:17 +0800

    vue3+vite

---
+++ src/views/about/About.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="About">
+  <div class="title">About</div>
+  <button class="button" @click="decrease">decrease</button>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { decrease } from '../../store'
+
+export default defineComponent({
+  name: 'About',
+  setup () {
+    return {
+      decrease
+    }
+  }
+})
+</script>
+<style lang="postcss">
+.About .title {
+  @apply text-green-500 text-2xl border-green-500 ml-4;
+}
+
+.About .button {
+  @apply rounded bg-green-700 text-white px-4 py-2 focus:ring-4;
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="Home">
+  Home
+  <button @click="increase">increase</button>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { increase } from '../../store'
+
+export default defineComponent({
+  name: 'Home',
+  setup () {
+    return {
+      increase
+    }
+  }
+})
+</script>
+<style lang="postcss">
+.home {
+  
+}
+</style>
